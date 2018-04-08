9667343d54729e12391c3f471817bc670a65f337
Daulet <daulet@zhanguzin.kz>
2018-04-07 19:16:05 -0700

    Add RedisAttribute

---
+++ src/Indigo.Functions.Redis/RedisAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.WebJobs.Description;
+using System;
+
+namespace Indigo.Functions.Redis
+{
+    [Binding]
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    public class RedisAttribute : Attribute
+    {
+        /// <summary>
+        /// Allows Redis config to be defined in app settings
+        /// </summary>
+        [AppSetting(Default = "RedisConfigurationOptions")]
+        public string Configuration { get; set; }
+    }
+}
---
+++ src/Indigo.Functions.Redis/RedisExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.WebJobs.Host.Config;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace Indigo.Functions.Redis
+{
+    public class RedisExtension : IExtensionConfigProvider
+    {
+        public void Initialize(ExtensionConfigContext context)
+        {
+            var rule = context.AddBindingRule<RedisAttribute>();
+
+            rule.WhenIsNotNull(nameof(RedisAttribute.Configuration))
+                .BindToInput(BuildConnectionFromAttributeAsync);
+        }
+
+        private async Task<IConnectionMultiplexer> BuildConnectionFromAttributeAsync(RedisAttribute attribute)
+        {
+            var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(attribute.Configuration);
+
+            return connectionMultiplexer;
+        }
+    }
+}
