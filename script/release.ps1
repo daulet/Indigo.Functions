cd7feb8d03852e488c700a37950e225b3506ea3f
Daulet <daulet@zhanguzin.kz>
2018-05-05 18:11:46 -0700

    Update release script to decouple release of different libraries

--- script/release.ps1
+++ script/release.ps1
@@ -1,17 +1,13 @@
 param(
-    [string]$rootPath,
+    [string]$libName,
     [string]$version
 )
 
 Write-Host "Running build" -ForegroundColor Blue
 
-dotnet msbuild $rootPath\src\indigo.functions.configuration\Indigo.Functions.Configuration.csproj '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /t:Rebuild /nologo /p:TreatWarningsAsErrors=true /p:Configuration=Release
-dotnet msbuild $rootPath\src\indigo.functions.injection\Indigo.Functions.Injection.csproj '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /t:Rebuild /nologo /p:TreatWarningsAsErrors=true /p:Configuration=Release
-dotnet msbuild $rootPath\src\indigo.functions.redis\Indigo.Functions.Redis.csproj '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /t:Rebuild /nologo /p:TreatWarningsAsErrors=true /p:Configuration=Release
+dotnet msbuild $PsScriptRoot\..\src\indigo.functions.$libName\Indigo.Functions.$libName.csproj '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /t:Rebuild /nologo /p:TreatWarningsAsErrors=true /p:Configuration=Release
 
 Write-Host "Publishing to Nuget" -ForegroundColor Blue
 
-$apiKey = Get-Content "$rootPath\..\vault\keys\nuget\indigo.functions.apikey"
-nuget push "$rootPath\src\indigo.functions.configuration\bin\Release\Indigo.Functions.Configuration.$version.nupkg" $apiKey -source https://api.nuget.org/v3/index.json
-nuget push "$rootPath\src\indigo.functions.injection\bin\Release\Indigo.Functions.Injection.$version.nupkg" $apiKey -source https://api.nuget.org/v3/index.json
-nuget push "$rootPath\src\indigo.functions.redis\bin\Release\Indigo.Functions.Redis.$version.nupkg" $apiKey -source https://api.nuget.org/v3/index.json
+$apiKey = Get-Content "$PsScriptRoot\..\..\vault\keys\nuget\indigo.functions.apikey"
+nuget push "$PsScriptRoot\..\src\indigo.functions.$libName\bin\Release\Indigo.Functions.$libName.$version.nupkg" $apiKey -source https://api.nuget.org/v3/index.json
